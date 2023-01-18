7eb9572a92c324f60632ec924a34226e1b64e28a
LommianLegacy <eggdozerat52@gmail.com>
2023-01-18 09:50:12 -0800

    h

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("Waltuh")
+x = input("pick a your favorite colour: ")
+if x == "red":
+  print("red is my favorite too")
+else:
+  print("ok")
+#1 question
+y = input("what is the capital of italy: ")
+if y == "rome":
+  print("correct")
+else:
+  print("incorrect")
+#waltuh
+z=input("what is the tempeture: ")
+if z <= '32':
+  print("nice and balmy")
+else:
+  print("BRRRRRR")
+#new
+V = input("when did washington become a state: ")
+if y == "1889":
+  print("correct")
+else:
+  print("incorrect")
+#new
+C=input("what is brandyns favorite car")
+if C== "prius":
+  print("correct")
+else:
+  print("No, Brandyn loves a light blue Prius in the fast lane")
